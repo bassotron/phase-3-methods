6ebadb172522d4e058c2df2d50a87d390d7947cb
Will Lovell <willard.lovell@icloud.com>
2022-08-23 12:28:21 -0600

    Completed with error

--- methods.rb
+++ methods.rb
@@ -1,1 +1,40 @@
-# Your code here!
+def greet_programmer
+	puts "Hello, programmer!"
+end
+
+
+def greet name
+	puts "Hello, #{name}!"
+end
+
+
+def greet_with_default name
+     if name == "Naureen"
+		 puts "Hello, #{name}!"
+	 elsif name == "Jimmy"
+		puts "Hello, #{name}!"
+	 else greet_with_default nil
+		puts "Hello, programmer!"
+	end
+	
+end 
+
+
+
+def add num1, num2
+	return num1 + num2
+end
+
+
+
+
+def halve number
+	if number != Integer
+	nil
+	elsif number == number
+	return	number / 2
+	end
+
+end
+
+
